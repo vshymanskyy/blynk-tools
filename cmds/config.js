14deaf9e3ec425fd2909511a3e7187f2c52eb9da
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2018-07-10 01:45:06 +0300

    Use tryParse

--- cmds/config.js
+++ cmds/config.js
@@ -6,37 +6,20 @@
 
 const fs = require('fs')
 const config = require('../lib/configstore.js')
-
-const JSON5 = require('json5')
+const { tryParse } = require('../lib/utils.js')
 
 module.exports = {
   command: 'config <name> [value]',
   describe: 'Set configuration',
   builder: (yargs) => {
     yargs
-      .example('$0 config server', "Display default server config")
-      .example('$0 config server.host test.com', "Set default server hostname")
-      .example('$0 config server \'{host:"test.com"}\'', 'Set default server (JSON)')
-      .example('$0 config device <auth>', 'Set default device')
-      /*.positional('name', {
-        describe: 'Parameter',
-        type: 'string',
-        hidden: true,
-      })*/
+      .example('$0 config default.server', "Display default server")
+      .example('$0 config default.device', "Display default device")
+      .example('$0 config default.server Blynk',    "Set default server name/hostname")
+      .example('$0 config default.device CoolDev1', "Set default device name/auth")
   },
   handler: main
 }
-
-const parsers = [
-  (val) => {
-    if (['undefined', '<undefined>', 'null'].includes(val)) {
-      return undefined
-    }
-    throw new Error()
-  },
-  (val) => JSON5.parse(val),
-  (val) => val,
-];
 
 function getConfig(name) {
   let val = config.get(name);
@@ -47,14 +30,7 @@ function getConfig(name) {
 function main(argv) {
   
   if (argv.value) {
-    let value;
-    
-    for (let parse of parsers) {
-      try {
-        value = parse(argv.value);
-        break;
-      } catch(e) {}
-    }
+    let value = tryParse(argv.value);
 
     config.set(argv.name, value)
 
