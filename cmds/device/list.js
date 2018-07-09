f7bac1b3ed567a790c537795bd784f7b291eea8f
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2018-07-10 02:37:13 +0300

    Add device list command

--- cmds/device/list.js
+++ cmds/device/list.js
@@ -7,32 +7,61 @@
 const chalk = require('chalk')
 const debug = require('debug')('BLE')
 
+const request = require('request-promise-native')
 const Table = require('cli-table-redemption');
+const Spinner = require('cli-spinner').Spinner;
+
+const config = require('../../lib/configstore.js')
 
 module.exports = {
   command: 'list',
   describe: 'List registered Blynk devices',
   builder: (yargs) => {
     yargs
-      .option({
-        auth: {
-          alias: ['token', 't'],
-          type: 'string',
-          describe: 'Auth token',
-          nargs: 1,
-        },
-      })
-      .example('$0 device add', 'Add device')
+      .example('$0 device list')
   },
   handler: main
 }
 
-function main(argv) {
-  var table = new Table({ chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''} });
-  table.push(
-      ['foo', 'bar', 'baz'],
-      ['frobnicate', 'bar', 'quuz']
-  );
+async function main(argv) {
+  var table = new Table({
+    head: ['Status', 'Name', 'Server', 'Token'],
+    style : { compact : true, 'padding-left' : 1, head: ['cyan', 'bold'] }
+  });
+
+  let devices = config.get('devices');
+
+  if (!devices.length) {
+    throw new Error(`No registered devices.\nYou can add a device using "blynk device add" command.`);
+  }
+
+  let defaultDevice;
+  try {
+    defaultDevice = config.findDevice(config.get('default.device'))
+  } catch(e) {}
+
+  let spinner = new Spinner(chalk.cyan.bold('%s') + '  Refreshing device status...');
+  spinner.start();
   
-  console.log(table.toString());
+  for (let d of devices) {
+    
+    const server = config.findServer(d.server);
+    const rejectUnauthorized = !server['http-api-insecure'];
+
+    let isDefault = defaultDevice && (defaultDevice.auth === d.auth);
+    let isConnected = await request({
+      uri: `${server.url}/${d.auth}/isHardwareConnected`,
+      json: true,
+      rejectUnauthorized,
+    });
+
+    let status = '';
+    status += isConnected ? chalk.green.bold('●') : '○';
+    status += isDefault ? chalk.cyan.bold('  ⇨') : '';
+    table.push([status, d.name, d.server, '...' + d.auth.slice(-6)])
+  }
+
+  spinner.stop();
+
+  console.log('\r' + table.toString());
 }
