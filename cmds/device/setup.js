98c0c94267a5e5439cd971abb2522d0f17c160ca
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2018-07-17 20:24:58 +0300

    Add WiFi Provisioning

---
+++ cmds/device/setup.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2017 Volodymyr Shymanskyy
+ **/
+
+'use strict';
+
+const chalk = require('chalk')
+const debug = require('debug')('Blynk')
+
+const config = require('../../lib/configstore.js')
+const { tryRequire } = require('../../lib/utils.js')
+const WiFiSetup = require('../../lib/wifi-setup')
+
+module.exports = {
+  command: 'setup <AP>',
+  describe: 'Provision Blynk device WiFi credentials',
+  builder: (yargs) => {
+    yargs
+      .option({
+        device: {
+          alias: ['d'],
+          type: 'string',
+          default: config.get('default.device'),
+          describe: 'Registered device name or auth token',
+          nargs: 1,
+        },
+        ssid: {
+          type: 'string',
+          describe: 'WiFi config: SSID (Access Point name)',
+          nargs: 1,
+        },
+        pass: {
+          type: 'string',
+          describe: 'WiFi config: password',
+          nargs: 1,
+        },
+      })
+      .demandOption(['device'])
+      .example('$0 device setup "Device WiFi AP" --device=Device1 --ssid="My Home WiFi" --pass="my_pass"', 'Setup a device')
+  },
+  handler: main
+}
+
+async function main(argv) {
+
+  if (!WiFiSetup) {
+    console.error(`Sorry, this command is not available on your OS yet.`);
+    process.exit(1);
+  }
+
+  const device = config.findDevice(argv.device);
+  const server = config.findServer(device.server);
+
+  const Spinner = require('cli-spinner').Spinner;
+  let spinner = new Spinner(chalk.cyan.bold('%s') + '  Setting up...');
+  spinner.start();
+
+  let result = await WiFiSetup.wifiSetup({
+    product: argv.AP,
+    ssid:    argv.ssid,
+    pass:    argv.pass,
+    auth:    device.auth,
+    server:  server['host'],
+    port:    server['hw-port-tcp'],
+  });
+  
+  spinner.stop();
+  
+  console.log('\rProvisioning complete. Device should appear online.');
+}
